3296000e2853018a849fea40d723394416b79950
AntonioFlavioSepulveda <148006207+AntonioFlavioSe@users.noreply.github.com>
2023-10-22 21:07:12 -0300

    Melhorias 1

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ function init() {
 //Função de comparação
 function compareNumbers() {
   const userNumber = Number(document.getElementById("inputBox").value);
-  userNumbers.push(' ' + userNumber);
+  userNumbers.push(" " + userNumber);
   document.getElementById("guesses").innerHTML = userNumbers;
+
+  //Criando os ifs do jogo:
+  if (userNumber > compareNumbers) {
+    document.getElementById("textOutput").innerHTML =
+      "Your number is too high.";
+    document.getElementById("inputBox").value = "";
+  } else if (userNumber < computerNumber) {
+    document.getElementById("textOutput").innerHTML = "Your number is too low.";
+    document.getElementById("inputBox").value = "";
+  } else {
+    document.getElementById("textOutput").innerHTML = "Yeah, congratulations!!";
+  }
 }

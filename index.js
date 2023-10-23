4df356ab48c3f8368edff11817d4db2139f6fac1
AntonioFlavioSepulveda <148006207+AntonioFlavioSe@users.noreply.github.com>
2023-10-22 21:14:45 -0300

    Criando limite de jogadas

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 let computerNumber;
 let userNumbers = [];
+let attemps = 0;
+let maxGuesses = 5;
 
 function init() {
   //Armazenando variaveis na variavel computerNumber:
@@ -13,16 +15,25 @@ function compareNumbers() {
   const userNumber = Number(document.getElementById("inputBox").value);
   userNumbers.push(" " + userNumber);
   document.getElementById("guesses").innerHTML = userNumbers;
-
-  //Criando os ifs do jogo:
-  if (userNumber > compareNumbers) {
-    document.getElementById("textOutput").innerHTML =
-      "Your number is too high.";
-    document.getElementById("inputBox").value = "";
-  } else if (userNumber < computerNumber) {
-    document.getElementById("textOutput").innerHTML = "Your number is too low.";
-    document.getElementById("inputBox").value = "";
-  } else {
-    document.getElementById("textOutput").innerHTML = "Yeah, congratulations!!";
+  if (attemps < maxGuesses) {
+    //Criando os ifs do jogo:
+    if (userNumber > compareNumbers) {
+      document.getElementById("textOutput").innerHTML =
+        "Your number is too high.";
+      document.getElementById("inputBox").value = "";
+      attemps++;
+      document.getElementById("attempts").innerHTML = attemps;
+    } else if (userNumber < computerNumber) {
+      document.getElementById("textOutput").innerHTML =
+        "Your number is too low.";
+      document.getElementById("inputBox").value = "";
+      attemps++;
+      document.getElementById("attempts").innerHTML = attemps;
+    } else {
+      document.getElementById("textOutput").innerHTML =
+        "Yeah, congratulations!!";
+      attemps++;
+      document.getElementById("attempts").innerHTML = attemps;
+    }
   }
 }
